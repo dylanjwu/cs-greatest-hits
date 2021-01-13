93342397d6a000267518613bab1e2f7350d6c15f
[dylanjwu] <dylanjwu@gmail.com>
2021-01-12 19:41:52 -0500

    add quick sort imp'l

--- algorithms/quick_sort.py
+++ algorithms/quick_sort.py
@@ -1,5 +1,36 @@
 # HARD
+# referenceed: https://www.techiedelight.com/quicksort/
 
-# pivoting
+def swap(a, i, j):
+    temp = a[i]
+    a[i] = a[j]
+    a[j] = temp
 
-# probablistic sorting technique
+def partition(a, start, end):
+
+    pivot = a[end]
+    p_index = start
+
+    for i in range(start, end):
+        if a[i] <= pivot:
+            swap(a, i, p_index)
+            p_index += 1
+
+    swap(a, p_index, end)
+    return p_index
+
+def quicksort(a, start, end):
+
+    if start >= end:
+        return
+
+    p_index = partition(a, start, end)
+
+    quicksort(a, start, p_index-1)
+    quicksort(a, p_index+1, end)
+
+arr = [9, -3, 5, 2, 6, 8, 8, -6, 1, 3]
+print(f'{arr}\n')
+quicksort(arr, 0, len(arr)-1)
+print(f'sorted: {arr})\n')
+
